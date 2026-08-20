1e038fcc4c0ce3f8ce30cb57958a9e42fc116a82
Andy Rabeson <hei.andy.19@gmail.com>
2026-08-20 15:44:34 +0300

    feat(repository): add repository

---
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+public class AccountRepository {
+    String url = "jdbc:postgresql://localhost:5432/transaction";
+    String password = "";
+    String user = "";
+
+    public List<Transaction> getAccount(){
+
+    }
+}
