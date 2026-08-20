c76b4830c4d9c85aca667ab5549ec543d791969e
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-20 16:13:56 +0300

    fix: adding a list of transactions in account

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @AllArgsConstructor
 @Builder
 @Data
 public class Account {
     private final String id;
     private final AccountType accountType;
+    private final List<Transaction> transactions;
 }
